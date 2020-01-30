233a83be85a08524b8798114fcf2a78052630934
Euan Ramsay <euan.ramsay@ros.gov.uk>
2020-01-30 11:05:17 +0000

    Working on auth0.

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,15 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import { decode, verify } from 'jsonwebtoken'
+
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
+import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('auth')
-
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-hw08mj11.eu.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +55,18 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  if (!jwt) throw new Error('Error with JSON Web Token')
+
+  try {
+    const keys = await Axios.get(jwksUrl)
+    const cert = keys[0]['x5c']
+
+    return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+  } catch (error) {
+    console.error(error)
+
+    return undefined
+  }
 }
 
 function getToken(authHeader: string): string {
