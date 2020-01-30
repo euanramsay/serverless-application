9993a427dd56249d4f51700926c575afb8b8ee82
Euan Ramsay <euan.ramsay@ros.gov.uk>
2020-01-30 16:00:00 +0000

    Working on DynamoDB setup.

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,5 +1,8 @@
 import 'source-map-support/register'
 
+import * as AWS from 'aws-sdk'
+import * as uuid from 'uuid'
+
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
@@ -7,13 +10,48 @@ import {
 } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { parseUserId } from '../../auth/utils'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
   console.log('TCL: newTodo', newTodo)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+  const itemId = uuid.v4()
+  const createdAt = new Date().toJSON()
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+
+  const newItem = {
+    todoId: itemId,
+    createdAt,
+    userId: parseUserId(jwtToken),
+    ...newTodo
+  }
+
+  await docClient
+    .put({
+      TableName: todosTable,
+      Item: newItem
+    })
+    .promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,15 +1,37 @@
 import 'source-map-support/register'
 
+import * as AWS from 'aws-sdk'
+
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyHandler,
   APIGatewayProxyResult
 } from 'aws-lambda'
 
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const todosTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  console.log('TCL: event', event)
-  return undefined
-  // TODO: Get all TODO items for a current user
+  console.log('Processing event: ', event)
+
+  const result = await docClient
+    .scan({
+      TableName: todosTable
+    })
+    .promise()
+
+  const items = result.Items
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
